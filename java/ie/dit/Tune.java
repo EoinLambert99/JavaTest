270ca57c2f4ff98558a662be5bc3735b0f8c3865
Eoin Lambert <eoinl085@gotmail.com>
2018-02-14 16:45:57 +0000

    Adding methods aswell as ArrayList

--- java/ie/dit/Tune.java
+++ java/ie/dit/Tune.java
@@ -1,1 +1,34 @@
 package ie.dit;
+
+public class Tune
+{
+
+	private int x;
+	private String title;
+	private String altTitle;
+	private String notation;
+
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+
+
+	/*public String toString()
+	{
+		Stringbuffer sb = new StringBuffer();
+		for (String tune:tunes)
+		{
+			sb.append(tune + ",");	
+		}
+
+		return sb.toString();
+	}
+*/
+
+}
--- java/ie/dit/TuneBook.java
+++ java/ie/dit/TuneBook.java
@@ -1,6 +1,47 @@
 package ie.dit;
 
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
+
 public class TuneBook
 {
+	ArrayList<String> tunes = new ArrayList<String>();
+
+	public void loadSongs()
+	{
+		BufferedReader inputStream = null;
+
+		try
+		{
+			inputStream = new BufferedReader(new FileReader("songs.txt"));
+            
+            String l;
+            while ((l = inputStream.readLine()) != null)
+            {
+                tunes.add(l);
+            }
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if(inputStream != null)
+			{
+				try
+				{
+					inputStream.close();
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+				
+			}
+		}
+	}
 	
 }
